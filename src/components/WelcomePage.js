a3895a0f2fcc47079bcfa6c640070faa84b6a371
Amir Helmy <amir.helmy@mail.mcgill.ca>
2019-04-10 09:50:45 -0700

    Remove unnecessary files, change npm start script to PORT 5500 and create WelcomePage component

---
+++ src/components/WelcomePage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function WelcomePage(props){
+    return(
+        <main>
+            <h1>Welcome to Habitz</h1>
+        </main>
+    );
+}
+
+export default WelcomePage
