cb262cbf03e3be101d8bdd5be2a9cdc96556bef1
Amir Helmy <amir.helmy@mail.mcgill.ca>
2019-04-13 00:51:32 -0700

    Style NavBar.js

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -12,32 +12,45 @@ function NavBar(props) {
 	}
 
 	return (
-		<nav
-			style={{
-				display: 'flex',
-				justifyContent: 'space-between',
-				padding: '10px 30px',
-			}}
-		>
-            <NavLink to="/">Home</NavLink>
-			<NavLink to="/habits/new">New Habit</NavLink>
-			<NavLink to="/habits">My Habits</NavLink>
-			<NavLink to="/habits/index_public">Public Habit Challenges</NavLink>
-			{!currentUser && (
-				<>
-					<NavLink to="/sign_in">Sign In</NavLink>
-					<NavLink to="/sign_up">Sign Up</NavLink>
-
-				</>
-			)}
-			{currentUser && (
-				<>
-					<span>{currentUser.full_name}</span>
-					<a href="#boo" onClick={handleSignOutClick}>
-            			Sign Out
-          			</a>
-				</>
-			)}
+		<nav className="navbar navbar-expand-sm navbar-dark bg-dark mb-2">
+			<NavLink className="navbar-brand text-success font-weight-bold" to="/">Habitz</NavLink>
+			<button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+          		<span className="navbar-toggler-icon"></span>
+        	</button>
+        	<div className="collapse navbar-collapse d-flex flex-row justify-content-end" id="navbarNavAltMarkup">
+          		<ul className="navbar-nav d-flex flex-row justify-content-end align-items-center">
+					<li className="nav-item mx-3">
+            			<NavLink to="/" className="text-success font-weight-bold">
+							Home <i class="fas fa-brain"></i>
+						</NavLink>
+					</li>
+					<li className="nav-item mx-3">
+						<NavLink to="/habits/new" className="text-success font-weight-bold">New Habit</NavLink>
+					</li>
+					<li className="nav-item mx-3">
+						<NavLink to="/habits" className="text-success font-weight-bold">My Habits</NavLink>
+					</li>
+					<li className="nav-item mx-3">
+						<NavLink to="/habits/index_public" className="text-success font-weight-bold">Public Habit Challenges</NavLink>
+					</li>
+					{currentUser && (
+						<>
+							<span className="navbar-item mx-3">{currentUser.full_name}</span>
+							<li className="nav-item mx-3">
+								<a href="#boo" onClick={handleSignOutClick}>Sign Out</a>
+							</li>
+						</>
+					)}
+					{!currentUser && (
+						<>
+							<li className="nav-item mx-3 ">
+								<NavLink to="/sign_in" className="btn btn-success mx-2 font-weight-bold">Sign In</NavLink>
+								<NavLink to="/sign_up" className="btn btn-success font-weight-bold">Sign Up</NavLink>
+							</li>
+						</>
+					)}
+				</ul>
+			</div>
 		</nav>
 	);
 }
